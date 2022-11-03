8833d050dc3232424bf37d3b96cd95465085ea0d
Jeremy Rand <jeremyrand@danwin1210.de>
2022-11-03 08:11:19 +0000

    p11: Make SecretKey an interface

--- p11/secret_key.go
+++ p11/secret_key.go
@@ -7,10 +7,20 @@ import "github.com/miekg/pkcs11"
 // responsibility to ensure that the object is actually a secret key. For
 // instance, if you use a FindObjects template that includes CKA_CLASS:
 // CKO_SECRET_KEY, you can be confident the resulting object is a secret key.
-type SecretKey objectImpl
+type SecretKey interface {
+	Decrypt(mechanism pkcs11.Mechanism, ciphertext []byte) ([]byte, error)
+	Encrypt(mechanism pkcs11.Mechanism, plaintext []byte) ([]byte, error)
+}
+
+// secretKeyImpl is an Object representing a secret (symmetric) key. Since the
+// SecretKey method can be called on any object, it is the user's
+// responsibility to ensure that the object is actually a secret key. For
+// instance, if you use a FindObjects template that includes CKA_CLASS:
+// CKO_SECRET_KEY, you can be confident the resulting object is a secret key.
+type secretKeyImpl objectImpl
 
 // Encrypt encrypts a plaintext with a given mechanism.
-func (secret SecretKey) Encrypt(mechanism pkcs11.Mechanism, plaintext []byte) ([]byte, error) {
+func (secret secretKeyImpl) Encrypt(mechanism pkcs11.Mechanism, plaintext []byte) ([]byte, error) {
 	s := secret.session
 	s.Lock()
 	defer s.Unlock()
@@ -26,7 +36,7 @@ func (secret SecretKey) Encrypt(mechanism pkcs11.Mechanism, plaintext []byte) ([
 }
 
 // Decrypt decrypts the input with a given mechanism.
-func (secret SecretKey) Decrypt(mechanism pkcs11.Mechanism, ciphertext []byte) ([]byte, error) {
+func (secret secretKeyImpl) Decrypt(mechanism pkcs11.Mechanism, ciphertext []byte) ([]byte, error) {
 	s := secret.session
 	s.Lock()
 	defer s.Unlock()
