0dfd80c4ec6c6fe9b7f857938543efb44bff219a
Jeremy Rand <jeremyrand@danwin1210.de>
2022-11-03 08:10:03 +0000

    p11: Make PublicKey and PrivateKey interfaces

--- p11/crypto.go
+++ p11/crypto.go
@@ -7,15 +7,34 @@ import "github.com/miekg/pkcs11"
 // the object is actually a public key. For instance, if you use a FindObjects
 // template that includes CKA_CLASS: CKO_PUBLIC_KEY, you can be confident the
 // resulting object is a public key.
-type PublicKey objectImpl
+type PublicKey interface {
+	Encrypt(mechanism pkcs11.Mechanism, plaintext []byte) ([]byte, error)
+	Verify(mechanism pkcs11.Mechanism, message, signature []byte)
+}
 
 // PrivateKey is an Object representing a private key. Since the PrivateKey
 // method can be called on any object, it is the user's responsibility to
 // ensure that the object is actually a private key.
-type PrivateKey objectImpl
+type PrivateKey interface {
+	Decrypt(mechanism pkcs11.Mechanism, ciphertext []byte)
+	Sign(mechanism pkcs11.Mechanism, message []byte)
+	Derive(mechanism pkcs11.Mechanism, attributes []*pkcs11.Attribute) ([]byte, error)
+}
+
+// PublicKey is an Object representing a public key. Since the PublicKey method
+// can be called on any object, it is the user's responsibility to ensure that
+// the object is actually a public key. For instance, if you use a FindObjects
+// template that includes CKA_CLASS: CKO_PUBLIC_KEY, you can be confident the
+// resulting object is a public key.
+type publicKeyImpl objectImpl
+
+// PrivateKey is an Object representing a private key. Since the PrivateKey
+// method can be called on any object, it is the user's responsibility to
+// ensure that the object is actually a private key.
+type privateKeyImpl objectImpl
 
 // Decrypt decrypts the input with a given mechanism.
-func (priv PrivateKey) Decrypt(mechanism pkcs11.Mechanism, ciphertext []byte) ([]byte, error) {
+func (priv privateKeyImpl) Decrypt(mechanism pkcs11.Mechanism, ciphertext []byte) ([]byte, error) {
 	s := priv.session
 	s.Lock()
 	defer s.Unlock()
@@ -31,7 +50,7 @@ func (priv PrivateKey) Decrypt(mechanism pkcs11.Mechanism, ciphertext []byte) ([
 }
 
 // Sign signs the input with a given mechanism.
-func (priv PrivateKey) Sign(mechanism pkcs11.Mechanism, message []byte) ([]byte, error) {
+func (priv privateKeyImpl) Sign(mechanism pkcs11.Mechanism, message []byte) ([]byte, error) {
 	s := priv.session
 	s.Lock()
 	defer s.Unlock()
@@ -46,7 +65,7 @@ func (priv PrivateKey) Sign(mechanism pkcs11.Mechanism, message []byte) ([]byte,
 	return out, nil
 }
 
-func (priv PrivateKey) deriveInner(mechanism pkcs11.Mechanism, attributes []*pkcs11.Attribute) (Object, error) {
+func (priv privateKeyImpl) deriveInner(mechanism pkcs11.Mechanism, attributes []*pkcs11.Attribute) (Object, error) {
 	s := priv.session
 	s.Lock()
 	defer s.Unlock()
@@ -63,7 +82,7 @@ func (priv PrivateKey) deriveInner(mechanism pkcs11.Mechanism, attributes []*pkc
 }
 
 // Derive derives a shared secret with a given mechanism.
-func (priv PrivateKey) Derive(mechanism pkcs11.Mechanism, attributes []*pkcs11.Attribute) ([]byte, error) {
+func (priv privateKeyImpl) Derive(mechanism pkcs11.Mechanism, attributes []*pkcs11.Attribute) ([]byte, error) {
 	sharedObj, err := priv.deriveInner(mechanism, attributes)
 	if err != nil {
 		return nil, err
@@ -78,7 +97,7 @@ func (priv PrivateKey) Derive(mechanism pkcs11.Mechanism, attributes []*pkcs11.A
 }
 
 // Verify verifies a signature over a message with a given mechanism.
-func (pub PublicKey) Verify(mechanism pkcs11.Mechanism, message, signature []byte) error {
+func (pub publicKeyImpl) Verify(mechanism pkcs11.Mechanism, message, signature []byte) error {
 	s := pub.session
 	s.Lock()
 	defer s.Unlock()
@@ -94,7 +113,7 @@ func (pub PublicKey) Verify(mechanism pkcs11.Mechanism, message, signature []byt
 }
 
 // Encrypt encrypts a plaintext with a given mechanism.
-func (pub PublicKey) Encrypt(mechanism pkcs11.Mechanism, plaintext []byte) ([]byte, error) {
+func (pub publicKeyImpl) Encrypt(mechanism pkcs11.Mechanism, plaintext []byte) ([]byte, error) {
 	s := pub.session
 	s.Lock()
 	defer s.Unlock()
